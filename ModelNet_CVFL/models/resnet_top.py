523758fae4a2612ab1bf8bb3d5a90afc3f3cffe2
userElaina <userelaina@pm.me>
2024-08-01 08:56:43 +0000

    :sparkles: change net

--- ModelNet_CVFL/models/resnet_top.py
+++ ModelNet_CVFL/models/resnet_top.py
@@ -28,6 +28,28 @@ class RESNET_top(nn.Module):
         pooled_view = self.classifier(x)
         return pooled_view
 
+from torch import nn, Tensor
+import torch.nn.functional as F
+from torch.nn import Module
+
+class Server_L2(Module):
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        embedding_size: int = 128
+    ):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, embedding_size)
+        self.fc2 = nn.Linear(embedding_size, output_size)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.softmax(x, dim=1)
+        return x
+
 
 def resnet_top(pretrained=False, **kwargs):
     r"""MVCNN model architecture from the
@@ -35,14 +57,15 @@ def resnet_top(pretrained=False, **kwargs):
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    model = RESNET_top(**kwargs)
-    if pretrained:
-        pretrained_dict = model_zoo.load_url(model_urls['alexnet'])
-        model_dict = model.state_dict()
-        # 1. filter out unnecessary keys
-        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict and v.shape == model_dict[k].shape}
-        # 2. overwrite entries in the existing state dict
-        model_dict.update(pretrained_dict)
-        # 3. load the new state dict
-        model.load_state_dict(model_dict)
-    return model
+    # model = RESNET_top(**kwargs)
+    # if pretrained:
+    #     pretrained_dict = model_zoo.load_url(model_urls['alexnet'])
+    #     model_dict = model.state_dict()
+    #     # 1. filter out unnecessary keys
+    #     pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict and v.shape == model_dict[k].shape}
+    #     # 2. overwrite entries in the existing state dict
+    #     model_dict.update(pretrained_dict)
+    #     # 3. load the new state dict
+    #     model.load_state_dict(model_dict)
+    # return model
+    return Server_L2()
