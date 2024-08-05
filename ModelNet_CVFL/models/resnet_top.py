76d4c8547a725fd58f3aea923416796996cc3e89
userElaina <userelaina@pm.me>
2024-08-05 14:06:24 +0000

    :sparkles: net

--- ModelNet_CVFL/models/resnet_top.py
+++ ModelNet_CVFL/models/resnet_top.py
@@ -12,42 +12,16 @@ model_urls = {
 
 
 class RESNET_top(nn.Module):
-
-    def __init__(self, num_classes=1000, num_clients=4):
+    def __init__(self, num_classes=1000, num_clients=4, embedding_size=128):
         super(RESNET_top, self).__init__()
-        self.classifier = nn.Sequential(
-            nn.Linear(16 * num_clients, num_classes),
-        )
-        #self.classifier = nn.Sequential(
-        #    nn.Linear(num_classes * num_clients, 100),
-        #    nn.Linear(100, 100),
-        #    nn.Linear(100, num_classes),
-        #)
+        self.fc1 = nn.Linear(16 * num_clients, embedding_size)
+        self.fc2 = nn.Linear(embedding_size, num_classes)
 
     def forward(self, x):
-        pooled_view = self.classifier(x)
-        return pooled_view
-
-from torch import nn, Tensor
-import torch.nn.functional as F
-from torch.nn import Module
-
-class Server_L2(Module):
-    def __init__(
-        self,
-        input_size: int,
-        output_size: int,
-        embedding_size: int = 128
-    ):
-        super().__init__()
-        self.fc1 = nn.Linear(input_size, embedding_size)
-        self.fc2 = nn.Linear(embedding_size, output_size)
-
-    def forward(self, x: Tensor) -> Tensor:
         x = self.fc1(x)
-        x = F.relu(x)
+        x = nn.functional.relu(x)
         x = self.fc2(x)
-        x = F.softmax(x, dim=1)
+        x = nn.functional.softmax(x, dim=1)
         return x
 
 
@@ -57,15 +31,14 @@ def resnet_top(pretrained=False, **kwargs):
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    # model = RESNET_top(**kwargs)
-    # if pretrained:
-    #     pretrained_dict = model_zoo.load_url(model_urls['alexnet'])
-    #     model_dict = model.state_dict()
-    #     # 1. filter out unnecessary keys
-    #     pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict and v.shape == model_dict[k].shape}
-    #     # 2. overwrite entries in the existing state dict
-    #     model_dict.update(pretrained_dict)
-    #     # 3. load the new state dict
-    #     model.load_state_dict(model_dict)
-    # return model
-    return Server_L2()
+    model = RESNET_top(**kwargs)
+    if pretrained:
+        pretrained_dict = model_zoo.load_url(model_urls['alexnet'])
+        model_dict = model.state_dict()
+        # 1. filter out unnecessary keys
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict and v.shape == model_dict[k].shape}
+        # 2. overwrite entries in the existing state dict
+        model_dict.update(pretrained_dict)
+        # 3. load the new state dict
+        model.load_state_dict(model_dict)
+    return model
